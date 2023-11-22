643822f02641b8a129c5022f4aab44713b4b12ae
Isabella1807 <megaoreomuffin@hotmail.com>
2023-11-22 11:16:11 +0100

    DB Setup

--- src/database/DB.js
+++ src/database/DB.js
@@ -1,1 +1,26 @@
-hej
+//Npm install firebase
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC86CO6SSjqIsQWBal7saC-hV_pAvQEY3M",
+    authDomain: "mortenschokolade.firebaseapp.com",
+    projectId: "mortenschokolade",
+    storageBucket: "mortenschokolade.appspot.com",
+    messagingSenderId: "427824695239",
+    appId: "1:427824695239:web:603ac1fec0ad477fa13be2",
+    measurementId: "G-18H1B9W5C1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore()
+
+
+export default db
