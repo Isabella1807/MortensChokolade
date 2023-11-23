2d09046dfff1d32d8a88929b54e563666f43307c
Isabella1807 <megaoreomuffin@hotmail.com>
2023-11-23 14:21:04 +0100

    show products in DB in ShopView

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,18 +1,26 @@
 <script setup>
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import productDB from "../database/products";
 
-onMounted(async () => {
-  console.log('JEG ER LIGE STARTET');
+let products = ref([]);
 
-  const allProducts = await productDB.getAllProducts('gris');
-  console.log(allProducts);
+onMounted(async () => {
+  products.value = await productDB.getAllProducts();
 });
+
 </script>
 
 <template>
   <h1>SHOP VIEW</h1>
-
+  <div>
+    <div>
+      <ul>
+        <li v-for="product in products">
+          {{product.title}}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
