21018e50382b6b832551cffae87758b3e9ff9f64
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2023-12-06 19:17:29 +0100

    upload af banner video

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -3,8 +3,45 @@
 </script>
 
 <template>
-  <img src="../assets/" alt="">
+  <div class="aboutMorten">
+    <img class="aboutMortenPortrait" src="../assets/portrait.png" alt="">
+    <p class="aboutMortenText">Morten flyttede til Odense og åbnede sin chokoladebutik i 2019, med en ambition om at
+      Odenseanerne skulle have
+      adgang til speciallavet chokolade, håndlavet på bæredygtig belgisk chokolade. <br> <br>
+
+      Chokoladen blev så populær, at der var nu blev forespørgsel på en webshop, hvor man kunne få leveret chokoladen
+      direkte hjem i stuen og man kan nu nyde Mortens lækre chokolade rundt i hele landet. <br> <br>
+
+      Morten har en baggrund indenfor grafisk design og har brugt sin viden til udformning af Mortens Chokolades brand med
+      inspiration fra modebranchen. <br> <br>
+
+      Der er fokus på kvalitet, bæredygtighed og smag. Samtidig, så bruger han sin chokolade til at udfolde sig kreativt,
+      så man får altid en unik smagsoplevelse, hver gang, uden at gå på kompromis med smag - Morten tester jævnligt sine
+      smage, for at sikre sig kvaliteten altid er i top.
+    </p>
+  </div>
   <h1>LANDING PAGE</h1>
 </template>
 
-<style scoped></style>
+<style scoped>
+.aboutMorten {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 5%;
+  background-color: #3B332C;
+  padding-top: 5%;
+  padding-bottom: 5%;
+  color: white;
+  margin-top: 15%;
+}
+
+.aboutMortenPortrait {
+  width: 30%;
+}
+
+.aboutMortenText {
+  width: 30%;
+  line-height: 170%
+}
+</style>
