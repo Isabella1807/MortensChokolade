5a3ddaf2c8235d3380273b8a05cc47f5c22acbc8
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2023-12-07 13:17:10 +0100

    tilføjelse af billede

--- src/components/bannerVideo.vue
+++ src/components/bannerVideo.vue
@@ -1,8 +1,28 @@
 <script setup>
+import { ref } from 'vue';
 
+const videoPath = "../assets/images/banner.mp4";
+
+const videoElement = ref();
+
+// Autoplay video when it's loaded
+const autoplayVideo = () => {
+    if (videoElement.value) {
+        videoElement.value.play().catch(error => {
+            console.error("Error playing video:", error);
+        });
+    }
+};
 </script>
+
 <template>
-    <video controls>
-        <source :src="../ assets / images / banner.mp4" type="video/mp4">
+    <video class="bannerVideo" controls ref="videoElement" @loadeddata="autoplayVideo" autoplay muted>
+        <source src="../assets/images/banner.mp4" type="video/mp4">
     </video>
 </template>
+
+<style>
+.bannerVideo {
+    width: 100%;
+}
+</style>
--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -4,6 +4,14 @@ import bannerVideo from '../components/bannerVideo.vue';
 
 <template>
   <bannerVideo />
+
+  <div class="landingScript">
+    <h1 class="landingScriptHeader">Prisvindende kvalitetschokolade</h1>
+    <p class="landingScripttext">Chokoladeværksted på Vintappestræde i Odense med både klassiske chokolader og innovative
+      smage,
+      håndlavet på bæredygtig belgisk chokolade.</p>
+  </div>
+
   <div class="aboutMorten">
     <img class="aboutMortenPortrait" src="../assets/portrait.png" alt="">
     <p class="aboutMortenText">Morten flyttede til Odense og åbnede sin chokoladebutik i 2019, med en ambition om at
@@ -21,10 +29,38 @@ import bannerVideo from '../components/bannerVideo.vue';
       smage, for at sikre sig kvaliteten altid er i top.
     </p>
   </div>
-  <h1>LANDING PAGE</h1>
+  <div>
+    <div>
+      <h2>Mortens chokolade blog</h2>
+    </div>
+    <div>
+      <img src="../assets/portrait.png" alt="">
+    </div>
+    <div>
+      <p>Julen i chokoladebutikken 2023
+        I år består Mortens Julekollektion af:
+        Mørk chokolade med pomerans
+        Mørk chokolade med </p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.landingScript {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  text-align: center;
+  margin-left: 25%;
+  line-height: 30px;
+  margin-top: 10%;
+}
+
+.landingScriptHeader {
+  margin-bottom: 10px;
+}
+
 .aboutMorten {
   display: flex;
   flex-direction: row;
@@ -34,15 +70,16 @@ import bannerVideo from '../components/bannerVideo.vue';
   padding-top: 5%;
   padding-bottom: 5%;
   color: white;
-  margin-top: 15%;
+  margin-top: 10%;
 }
 
 .aboutMortenPortrait {
-  width: 30%;
+  width: 35%;
+  height: fit-content;
 }
 
 .aboutMortenText {
   width: 30%;
-  line-height: 170%
+  line-height: 30px;
 }
 </style>
