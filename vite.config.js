32e2a983b6eca60a22dfc55b5d862f5fb8af6f7d
Isabella1807 <megaoreomuffin@hotmail.com>
2023-12-11 20:32:43 +0100

    vite config change so that index dont have to be in root in filezilla

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MortensChokolade/', //VERY IMPORTNANT, hvis projektet(index.html) ikke er i root i filezilla - skal det her være med!
   plugins: [
     vue(),
   ],
